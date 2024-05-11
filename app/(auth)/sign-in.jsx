3add81e332b7285ac8296d27213a002c55fa1241
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-05-11 12:28:28 +0700

    add pages: intro, sign up sign in

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,12 +1,70 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, Dimensions, Alert, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import { Link, router } from "expo-router";
+
+import { CustomButton, FormField } from "../../components";
+import { useState } from "react";
 
 const SignIn = () => {
-  return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
-  )
-}
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
 
-export default SignIn
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const submit = () => {
+    
+  }
+
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full flex justify-center min-h-[85vh] px-4 my-6">
+          <Image source={images.logo} className="w-[185px] h-[50px]" />
+          <Text className="text-2xl font-semibold text-white mt-10 font-psemibold mb-7">
+            Sign In to SoundCharm
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            containerStyles="mt-7"
+            handlePress={isSubmitting}
+            
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have an account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Sign up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import {Link} from 'expo-router';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, Text, View, Image } from "react-native";
+import { Redirect, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
-export default function Login() {
+export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className = "text-3xl font-pblack">SOUNDCHARM</Text>
-      <StatusBar style="auto" />
-      <Link href = "/home" style = {{color: 'blue'}}>Go to home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center min-h-[85vh] px-10 content-start">
+          <Image source={images.logo} className="h-[100px] w-full" />
+
+          <View className="h-[350px] w-[80%] rounded-lg shadow-md border border-white">
+            <Image source={images.cards} className="w-full h-full rounded-lg" />
+          </View>
+
+          <View className="relative mt-3">
+            <Text className="text-3xl text-white font-bold text-center relative ">
+              Discover endless songs with
+              <Text className="text-secondary-200"> SoundCharm</Text>
+              <Image
+                source={images.path}
+                className="w-[286px] h-[28px]"
+                resizeMode="contain"
+              />
+            </Text>
+
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+              Embark on a Musical Odyssey: Dive into a World of Boundless Discovery with SoundCharm
+            </Text>
+          </View>
+
+          <CustomButton
+            title="Continue with email"
+            handlePress = {() => {router.push('/sign-in')}}
+            containerStyles = "w-full mt-7"
+
+          />
+
+          
+
+        </View>
+
+      </ScrollView>
+      <StatusBar hidden/>
+    </SafeAreaView>
   );
 }
-
-
